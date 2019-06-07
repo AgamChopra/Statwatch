a44fe7a0fda69d67d1b5d0b9c453b1647903170d
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-06 23:39:35 -0400

    refactor: added container and typography

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,19 +5,24 @@ import MockApi from './api/MockApi';
 import StatList from './components/StatList';
 import Button from '@material-ui/core/Button';
 import NavigationBar from './components/NavigationBar';
-
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 class App extends Component {
   private api: StatwatchApi = new MockApi();
 
   render() {
     return (
-      <div className="App">
-        <NavigationBar />
-        <StatList stats={this.api.getStats()} />
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </div>
+      <Container maxWidth="md">
+        <Typography component="div" style={{ position: 'absolute', top: '80px', backgroundColor: '#cfe8fc' }}>
+          <div className="App">
+            <NavigationBar />
+            <StatList stats={this.api.getStats()} />
+            <Button variant="contained" color="primary">
+              Hello World
+            </Button>
+          </div>
+        </Typography>
+      </Container>
     );
   }
 }
